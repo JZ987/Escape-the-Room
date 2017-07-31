c51ee803316ee55aeae407e0ba6fbcd47e14f4b9
Demo User <demouser@MacBook-Air.local>
2017-07-31 16:37:38 -0400

    login

--- main.py
+++ main.py
@@ -17,6 +17,8 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.api import users
+
 
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -39,9 +41,29 @@ class InstructionsHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('templates/instructions.html')
         self.response.write(template.render(template_vars))
 
+class HighScoreHandler(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {}
+        template = jinja_environment.get_template('templates/highscore.html')
+        self.response.write(template.render(template_vars))
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                users.create_login_url('/'))
+
+        self.response.write('<html><body>%s</body></html>' % greeting)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/game', GameHandler),
     ('/instructions', InstructionsHandler)
+    ('/highscore', HighScoreHandler)
+    ('/login, MainPage')
 ], debug=True)
